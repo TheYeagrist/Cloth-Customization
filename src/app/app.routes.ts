67ed34817947e83e8f02f638a55aebd4cbeb2632
TheYeagrist <ncjatt1@gmail.com>
2024-06-25 17:33:00 +0530

    Landing page done

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+const routes : Routes{
+    
+}
+
+export const routes: Routes = [];
